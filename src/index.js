5f71fc3fb8f103b488b40a20ba2e41227bea92cc
waweru-sdf <felixwaweru200@gmail.com>
2025-06-27 12:45:58 +0300

    post for my first form

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function renderOnepost(post){
     postcard.innerHTML=`
     <img src="${post.imageUrl}"/>
     <div class="content">
-     <h2>${post.country}</p>
+     <h2>${post.country}</h3>
      <p>${post.description}</p>
     </div>
     `;
@@ -19,3 +19,35 @@ function getAllPosts(){
 document.addEventListener("DOMContentLoaded", function(){
     getAllPosts();
 })
+
+document.getElementById('form-post').addEventListener('submit', function(e) {
+  e.preventDefault(); // prevent page reload
+
+  const newPost = {
+    country: document.getElementById('visited-country').value,
+    imageUrl: document.getElementById('image-url').value,
+    description: document.getElementById('description').value
+  };
+
+  // POST to the server
+  fetch('http://localhost:3000/post', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(newPost)
+  })
+  .then(response => response.json())
+  .then(addedPost => {
+    // now addedPost is just like the ones from your GET
+    renderOnePost(addedPost);  // reuse your innerHTML function here!
+  });
+});
+
+/*function renderOneFavorite(favorite){
+    let favoritecard= document.createElement('div');
+    favoritecard.className= 'favorite-card';
+    favoritecard.innerHTML=`
+    `
+
+}
